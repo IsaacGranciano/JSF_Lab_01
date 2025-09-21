6c8147f0a4369766a8254087b479babfed2ac7bc
IsaacGranciano <200592217@student.georgianc.on.ca>
2025-09-21 17:40:27 -0400

    Update R_File.js

--- Assigment_01/R_File.js
+++ Assigment_01/R_File.js
@@ -5,15 +5,16 @@ const fs = require ('node:fs');
 //This is the function that is reading the text file
 fs.readFile('Text.txt', 'utf8',(err, data) => {
 
-//This functions is working with a loop 
+//This functions is working with a if statement 
     if (err){
-    //If the loop could not find a text will show "Text could not be read"
+    //If the statement could not find a text will show "Text could not be read"
         console.error("Text could not be read", err,);
         return;
     }
-    //If the loop finds a text will show the paragraph into Text.txt
+    //If the statement finds a text will show the paragraph into Text.txt
     console.log(" ---------------------\n", "| This is the text: |\n", "---------------------");
     console.log(data);
 });
+
 
 
